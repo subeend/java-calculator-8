ec6be59de948da77f4585de8a76a39a988c61d34
subeend <happysong98@gmail.com>
2025-10-20 01:55:24 +0900

    feat: 커스텀 헤더 점검

--- src/main/java/calculator/StringAddCalculator.java
+++ src/main/java/calculator/StringAddCalculator.java
@@ -1,13 +1,25 @@
 package calculator;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public final class StringAddCalculator {
     private StringAddCalculator() {}
+
+    private static final Pattern CUSTOM_HEADER = Pattern.compile("//(.)\\n(.*)", Pattern.DOTALL);
 
     public static int add(String input){
         if(input == null || input.isBlank()){
             return 0;
         }
         input = input.trim();
+
+        if(input.startsWith("//")){
+            Matcher m = CUSTOM_HEADER.matcher(input);
+            if(!m.matches()){
+                throw new IllegalArgumentException("올바르지 않은 형태의 문자열이에요.");
+            }
+        }
 
         throw new IllegalArgumentException("올바르지 않은 형태의 문자열이에요.");
     }
